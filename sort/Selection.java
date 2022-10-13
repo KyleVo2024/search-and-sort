d54adf6b37714dfd4c7307164beca39e07c03187
Kyle Vo <kyle.vo@mrjaffesclass.com>
2022-10-13 12:44:22 -0700

    AHHHHH

--- sort/Selection.java
+++ sort/Selection.java
@@ -20,30 +20,27 @@ public class Selection
     int winner = arr2[0];
     int holder;
     int postion= 0;
-    for (int i=0; i<arr2.length; i++) {
+    for (int i=0; i<arr2.length-1; i++) {
         for (int a=i; a<arr2.length-1; a++) {
             if(arr2 [a+1] < arr2 [a] && arr2 [a+1] < winner){
                 winner = arr2 [a+1]; 
                 postion = a+1;
             }
         }
-        holder = arr2[i]; 
-        arr2 [i] = winner; 
-        arr2 [postion] = holder;
-        if (i==9){
-            winner = arr2 [i];
-        }else{
-            winner = arr2 [i+1];
+        for (int k=postion; k<0; k--) {
+            arr2 [k] = arr [k-1];
         }
+        arr2 [i] = winner; 
+        winner = arr2 [i+1];
     }
     return arr2;
   }
   
   public static void main(String[] args) {
-    int [] arr = {10,9,8,7,6,5,4,3,2,1};
-      /*int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-    31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-    73,54,51,25,33,20,52,79,97,70,54,63,49};*/ 
+    //int [] arr = {10,9,8,7,6,5,4,3,2,1};
+    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};
     
     // Test the sort
     testSort(sort(arr));
