a91c2c713d4fe71d61f80aabc889ce80064ee070
Kyle Vo <kyle.vo@mrjaffesclass.com>
2022-10-18 12:40:49 -0700

    help me

--- sort/Selection.java
+++ sort/Selection.java
@@ -27,9 +27,11 @@ public class Selection
                 postion = a+1;
             }
         }
-        for (int k=postion; k<0; k--) {
+        holder = arr [i];
+        for (int k=postion; k>postion+i; k--) {
             arr2 [k] = arr [k-1];
         }
+        arr[i+1] = holder;
         arr2 [i] = winner; 
         winner = arr2 [i+1];
     }
@@ -37,11 +39,11 @@ public class Selection
   }
   
   public static void main(String[] args) {
-    //int [] arr = {10,9,8,7,6,5,4,3,2,1};
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+    int [] arr = {10,9,8,7,6,5,4,3,2,1};
+    int[] arr2 = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
                 31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
                 73,54,51,25,33,20,52,79,97,70,54,63,49};
-    
+    // 1,4,3,2,1
     // Test the sort
     testSort(sort(arr));
   }
